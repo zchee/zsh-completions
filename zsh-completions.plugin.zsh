008cf7a16b481f88a6d2155e2dafa9e84815cd94
lainiwa <26632061+lainiwa@users.noreply.github.com>
2020-06-19 06:31:27 +0300

    Update zsh-completions.plugin.zsh

--- zsh-completions.plugin.zsh
+++ zsh-completions.plugin.zsh
@@ -1,7 +1,6 @@
+#!/usr/bin/env zsh
 
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-if [[ $PMSPEC != *f* ]] {
-    fpath=( "${0:h}/src/macOS" "${0:h}/src/go" "${0:h}/src/zsh" "${fpath[@]}" )
-}
+fpath=( "${0:h}/src/macOS" "${0:h}/src/go" "${0:h}/src/zsh" "${fpath[@]}" )
