15efc4bd3f2780bc32a4f94058ebdec934d17dac
Koichi Shiraishi <zchee.io@gmail.com>
2026-07-10 00:55:19 +0900

    plugin.zsh: update fpath to add ai and rust

--- zsh-completions.plugin.zsh
+++ zsh-completions.plugin.zsh
@@ -3,4 +3,4 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-fpath=( "${0:h}/src/macOS" "${0:h}/src/go" "${0:h}/src/zsh" "${fpath[@]}" )
+fpath=( "${0:h}/src/ai" "${0:h}/src/go" "${0:h}/src/macOS" "${0:h}/src/rust" "${0:h}/src/zsh" "${fpath[@]}" )
